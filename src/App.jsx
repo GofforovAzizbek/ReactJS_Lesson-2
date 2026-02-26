7f97048a0d8ed5ab4271c7403876e76dcca4db28
GofforovAzizbek <gofforovazizbek2024@gmail.com>
2026-02-26 17:51:42 +0500

    Four Commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,17 @@ import cards6 from "./assets/card-6.png";
 
 import right from "./assets/right.svg";
 
+import footerlogo from "./assets/footerlogo.svg";
+
+import fb from "./assets/fb.svg";
+import instagram from "./assets/instagram.svg";
+import twitter from "./assets/twitter.svg";
+import linkedin from "./assets/linkedin.svg";
+import yt from "./assets/yt.svg";
+
+import net from "./assets/net.svg";
+import arrowfooter from "./assets/arrowfooter.svg";
+
 const App = () => {
   return (
     <>
@@ -106,6 +117,7 @@ const App = () => {
         </div>
       </header>
 
+      {/* Main */}
       <main className="">
         {/* Hero Section */}
         <section className="pt-[54px]">
@@ -229,7 +241,234 @@ const App = () => {
             </a>
           </div>
         </section>
+
+        {/* Section Contact */}
+        <section className="py-[100px] bg-[#F4F9FC]">
+          <div className="max-w-[1180px] w-[100%] mx-auto px-[20px]">
+            <div className="flex items-center justify-between">
+              <div className="max-w-[360px] w-[100%]">
+                <span className="font-hind-siliguri font-medium text-[14px] leading-[100%] tracking-[14.5%] text-center uppercase text-[#C72535]">
+                  User Contact
+                </span>
+                <h3 className="font-hind-siliguri font-medium text-[25px] leading-[100%] tracking-[0%] text-[#030D15] pt-[8px] pb-[14px]">
+                  Contact Us for those interested.
+                </h3>
+                <p className="font-hind-siliguri font-normal text-[14px] leading-[21px] tracking-[0%] text-[#030D1590]">
+                  The standard chunk of Lorem Ipsum used since the is reproduced
+                  below for those interested.
+                </p>
+              </div>
+
+              <div className="flex items-center">
+                <input
+                  className="font-hind-siliguri font-normal text-[14px] leading-[21px] tracking-[0%] text-[#030D1590] bg-[#fff] placeholder:text-[#030D1590] outline-0 border-[1px] border-[#030d152f] py-[15px] pr-[160px] pl-[25px]"
+                  type="email"
+                  placeholder="Enter your email address"
+                />
+                <button
+                  type="submit"
+                  className="py-[19.5px] px-[24px] bg-[#247CC0] font-hind-siliguri text-[14px] leading-[100%] text-[#fff] hover:opacity-[0.7]"
+                >
+                  SUBSCRIBE
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
+
+      {/* Footer */}
+      <footer className="pt-[75px] pb-[55px] bg-[#030D15]">
+        <div className="max-w-[1180px] w-[100%] mx-auto px-[20px]">
+          <div className="flex justify-between">
+            <div className="flex flex-col gap-[20px] max-w-[260px] w-[100%]">
+              <a href="/" className="">
+                <img src={footerlogo} alt="" className="" />
+              </a>
+              <p className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]">
+                STORE - worldwide fashion store since 1978. We sell over 1000+
+                branded products on our web-site.
+              </p>
+            </div>
+            <div className="">
+              <h4 className="font-hind-siliguri font-semibold text-[16px] leading-[100%] tracking-[0%] text-[#fff] mb-[25px]">
+                INFORMATION
+              </h4>
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff] mb-[15px]"
+              >
+                New Collection
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff] mb-[15px]"
+              >
+                About Store
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff] mb-[15px]"
+              >
+                Contact Us
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff] mb-[15px]"
+              >
+                Latest News
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff] mb-[15px]"
+              >
+                Our Sitemap
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                Orders History
+              </a>
+              <br />
+            </div>
+            <div className="">
+              <h4 className="font-hind-siliguri font-semibold text-[16px] leading-[100%] tracking-[0%] text-[#fff] mb-[25px]">
+                FOOTER MENU
+              </h4>
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                Instagram profile
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                New Collection
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                Contact Us
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                Latest News
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                Terms & Conditions
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                Purchase Theme
+              </a>
+              <br />
+            </div>
+            <div className="">
+              <h4 className="font-hind-siliguri font-semibold text-[16px] leading-[100%] tracking-[0%] text-[#fff] mb-[25px]">
+                USEFUL LINKS
+              </h4>
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                Instagram profile
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                New Collection
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                Contact Us
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                Latest News
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                Terms & Conditions
+              </a>
+              <br />
+              <a
+                href="#"
+                className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                Purchase Theme
+              </a>
+              <br />
+            </div>
+            <div className="">
+              <h4 className="font-hind-siliguri font-semibold text-[16px] leading-[100%] tracking-[0%] text-[#fff] mb-[25px]">
+                ABOUT THE STORE
+              </h4>
+              <p className="font-hind-siliguri font-normal text-[14px] leading-[100%] tracking-[0%] text-[#fff] max-w-[260px] w-[100%] mt-[25px] mb-[50px]">
+                STORE - worldwide fashion store since 1978. We sell over 1000+
+                branded products on our web-site.
+              </p>
+              <div className="flex items-center gap-[25px] mb-[50px]">
+                <a href="#">
+                  <img src={fb} alt="" className="" />
+                </a>
+                <a href="#">
+                  <img src={instagram} alt="" className="" />
+                </a>
+                <a href="#">
+                  <img src={twitter} alt="" className="" />
+                </a>
+                <a href="#">
+                  <img src={linkedin} alt="" className="" />
+                </a>
+                <a href="#">
+                  <img src={yt} alt="" className="" />
+                </a>
+              </div>
+
+              <a
+                href="#"
+                className="flex items-center font-hind-siliguri font-medium text-[14px] leading-[100%] tracking-[0%] text-[#fff]"
+              >
+                <img src={net} alt="" className="mr-[8px]" />
+                English
+                <img src={arrowfooter} alt="" className="ml-[4px]" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
